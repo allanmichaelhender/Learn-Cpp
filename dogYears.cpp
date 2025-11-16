dec039b013ed3e20d614e96fb88aff599c44d207
Allan <allanhender@gmail.com>
2025-11-16 17:38:12 +0000

    creating a dog to human years calculator

---
+++ dogYears.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+  
+  int dog_age = 8;
+  int early_years;
+  int later_years;
+  int human_years;
+
+  early_years = 21;
+  later_years = (dog_age-2)*4;
+  human_years = early_years + later_years;
+
+  std::cout << "Age of dog in dog years is: " << dog_age << ", in human years this equals: " << human_years << ".\n";
+}
